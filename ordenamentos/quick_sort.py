689af4d8ff5b3d3b6b8f2328602ce275fa95b80c
light <caiocr610@gmail.com>
2023-08-17 21:55:03 -0300

    corrigindo quick_sort e adicionando bubble_sort

--- ordenamentos/quick_sort.py
+++ ordenamentos/quick_sort.py
@@ -7,5 +7,9 @@ def ordinate(array):
     middle = [x for x in array if x == array[pivot]]
     right = [x for x in array if x > array[pivot]]
 
-    return ordinate(left) + ordinate(middle) + ordinate(right)
+    return ordinate(left) + middle + ordinate(right)
 
+arr = [4, 32, 42, 19, 16, 90, 74, 44, 6, 99, 89, 26, 23, 45, 94, 61, 98, 97, 69, 55, 86, 58, 90, 20, 55, 6, 62, 28, 32, 28, 34, 57, 92, 22, 80, 63, 96, 82, 61, 67, 34, 88, 60, 45, 24, 45, 94, 84, 17, 1]
+
+array_ordenado = ordinate(arr)
+print(array_ordenado)
